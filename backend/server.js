69c66a90aedcb4ccafcfcb9a62182f5ac4038825
unknown <selimhalilaytb@gmail.com>
2025-03-21 15:27:16 +0100

    masquer node_modules

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,8 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB", err));
+
+  
 // Routes
 app.use("/api/tasks", taskRoutes);
 
